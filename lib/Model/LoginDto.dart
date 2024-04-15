2950d5515bbc0713e08495a538b2f50f25446399
Andres Felipe Rios Bolivar <sql.programador29@opa.com.co>
2024-04-14 19:54:58 -0500

    Implement Generic Models

---
+++ lib/Model/LoginDto.dart
@@ -0,0 +1,24 @@
+import 'package:vetadminconnectmobile/Model/Generic/BaseModel.dart';
+
+class LoginDto extends BaseModel<LoginDto> {
+  final String email;
+  final String password;
+
+  LoginDto({required this.email, required this.password});
+
+  @override
+  Map<String, Object?> toJson() {
+    return {
+      'email': email,
+      'password': password,
+    };
+  }
+
+  @override
+  LoginDto fromJson(Map<String, Object?> json) {
+    return LoginDto(
+      email: json['email'] as String,
+      password: json['password'] as String,
+    );
+  }
+}
