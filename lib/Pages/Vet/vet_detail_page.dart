ab4112fd760cbc2b29411459b0037c77c81664c6
Andres Felipe Rios Bolivar <sql.programador29@opa.com.co>
2024-05-24 00:42:04 -0500

    Improvements - Vet detail page with modal comment page - Vet search page with lazy loading

--- lib/Pages/Vet/vet_detail_page.dart
+++ lib/Pages/Vet/vet_detail_page.dart
@@ -1,21 +1,34 @@
 import 'dart:math';
-
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:vetadminconnectmobile/Model/Vet.dart';
 
-class VeterinarioDetailsPage extends StatelessWidget {
+class VeterinarioDetailsPage extends StatefulWidget {
   final Vet veterinario;
 
   const VeterinarioDetailsPage({Key? key, required this.veterinario})
       : super(key: key);
 
   @override
+  _VeterinarioDetailsPageState createState() => _VeterinarioDetailsPageState();
+}
+
+class _VeterinarioDetailsPageState extends State<VeterinarioDetailsPage> {
+  double _rating = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _rating = Random().nextDouble() * 5;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detalles de ${veterinario.fullName}'),
+        title: Text('Detalles de ${widget.veterinario.fullName}'),
       ),
       body: Center(
         child: Card(
@@ -41,7 +54,7 @@ class VeterinarioDetailsPage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      veterinario.fullName,
+                      widget.veterinario.fullName,
                       style: const TextStyle(
                         fontSize: 24,
                         fontWeight: FontWeight.bold,
@@ -54,7 +67,7 @@ class VeterinarioDetailsPage extends StatelessWidget {
                         const Icon(Icons.phone_android, size: 16),
                         const SizedBox(width: 5),
                         Text(
-                          'Telefono: ${veterinario.phoneNumber}',
+                          'Telefono: ${widget.veterinario.phoneNumber}',
                           style: const TextStyle(fontSize: 16),
                         ),
                       ],
@@ -65,7 +78,7 @@ class VeterinarioDetailsPage extends StatelessWidget {
                         const Icon(Icons.pets_outlined, size: 16),
                         const SizedBox(width: 5),
                         Text(
-                          'Especialidad: ${veterinario.vetSpecialities.isEmpty ? '' : veterinario.vetSpecialities.first.name}',
+                          'Especialidad: ${widget.veterinario.vetSpecialities.isEmpty ? '' : widget.veterinario.vetSpecialities.first.name}',
                           style: const TextStyle(fontSize: 16),
                         ),
                       ],
@@ -76,7 +89,7 @@ class VeterinarioDetailsPage extends StatelessWidget {
                         const Icon(Icons.wc_outlined, size: 16),
                         const SizedBox(width: 5),
                         Text(
-                          'Ubicación: ${veterinario.cityName}',
+                          'Ubicación: ${widget.veterinario.cityName}',
                           style: const TextStyle(fontSize: 16),
                         ),
                       ],
@@ -91,7 +104,7 @@ class VeterinarioDetailsPage extends StatelessWidget {
                       ),
                     ),
                     RatingBar.builder(
-                      initialRating: Random().nextDouble() * 5,
+                      initialRating: _rating,
                       minRating: 1,
                       direction: Axis.horizontal,
                       allowHalfRating: false,
@@ -103,24 +116,23 @@ class VeterinarioDetailsPage extends StatelessWidget {
                         color: Colors.amber,
                       ),
                       onRatingUpdate: (rating) {
-                        print(rating);
-                        // Hacer algo con la calificación
+                        setState(() {
+                          _rating = rating;
+                        });
+                        _showRatingDialog(context, rating);
                       },
                     ),
                     const SizedBox(height: 10),
                     // Botón de WhatsApp
-                    ElevatedButton.icon(
+                    IconButton(
                       onPressed: () {
-                        _launchWhatsApp(veterinario.phoneNumber, context);
+                        _launchWhatsApp(widget.veterinario.phoneNumber, context);
                       },
-                      icon: Icon(
-                        Icons.message,
-                        color: Colors.green.shade500,
+                      icon: Image.asset(
+                        'assets/images/whatsapp_logo.png', // Replace with your actual image filename
+                        width: 50, // Adjust the size as needed
+                        height: 50, // Adjust the size as needed
                       ),
-                      label: Text(
-                        'Enviar mensaje por WhatsApp',
-                        style: TextStyle(color: Colors.green.shade500),
-                      ), // Utiliza el color principal de tu app
                     ),
                   ],
                 ),
@@ -134,18 +146,18 @@ class VeterinarioDetailsPage extends StatelessWidget {
 
   _launchWhatsApp(String phoneNumber, BuildContext context) async {
     final phone = phoneNumber.replaceAll(RegExp(r'[^\d]'), '');
-    String whatsappURlAndroid =
-        "whatsapp://send?phone=$phone&text="
+    String whatsappURlAndroid = "whatsapp://send?phone=$phone&text="
         "Estoy interesado en agendar una consulta:";
-      if (whatsappURlAndroid.isNotEmpty) {
-        await launchUrl(Uri.parse(whatsappURlAndroid));
-      }else {
+    if (whatsappURlAndroid.isNotEmpty) {
+      await launchUrl(Uri.parse(whatsappURlAndroid));
+    } else {
       // Handle no WhatsApp installed scenario
       showDialog(
         context: context,
         builder: (context) => AlertDialog(
           title: const Text("WhatsApp no está instalado"),
-          content: const Text("Necesitas tener WhatsApp instalado para enviar mensajes."),
+          content: const Text(
+              "Necesitas tener WhatsApp instalado para enviar mensajes."),
           actions: [
             TextButton(
               onPressed: () => Navigator.pop(context),
@@ -155,5 +167,99 @@ class VeterinarioDetailsPage extends StatelessWidget {
         ),
       );
     }
-}
+  }
+
+  void _showRatingDialog(BuildContext context, double rating) {
+    final _formKey = GlobalKey<FormState>();
+    final _commentController = TextEditingController();
+
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: const Text('Calificación'),
+              content: Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      RatingBar.builder(
+                        initialRating: rating,
+                        minRating: 1,
+                        direction: Axis.horizontal,
+                        allowHalfRating: false,
+                        itemCount: 5,
+                        itemSize: 30,
+                        itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                        itemBuilder: (context, _) =>
+                        const Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        onRatingUpdate: (newRating) {
+                          setState(() {
+                            rating = newRating;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 10),
+                      TextFormField(
+                        controller: _commentController,
+                        decoration: const InputDecoration(
+                          labelText: 'Comentario',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'El comentario es obligatorio';
+                          }
+                          return null;
+                        },
+                        maxLines: 3,
+                      ),
+                    ],
+                  ),
+                )
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancelar'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      final comment = _commentController.text;
+                      print('Calificación: $rating');
+                      print('Comentario: $comment');
+
+                      // Simulación de envío al servidor
+
+                      Navigator.pop(context); // Cerrar el diálogo
+                      _showSnackBar(rating); // Mostrar snackbar
+                    }
+                  },
+                  child: const Text('Enviar'),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  void _showSnackBar(double rating) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text(
+          'Calificación registrada con éxito',
+        ),
+      ),
+    );
+  }
 }
--- lib/Pages/Vet/vet_search_page.dart
+++ lib/Pages/Vet/vet_search_page.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:vetadminconnectmobile/Model/PaginationDto.dart';
 import 'package:vetadminconnectmobile/Model/Vet.dart';
 import 'package:vetadminconnectmobile/Pages/Vet/vet_detail_page.dart';
@@ -15,11 +17,12 @@ class VetSearchPage extends StatefulWidget {
 }
 
 class _VetSearchPageState extends State<VetSearchPage> {
-  var pagination = PaginationDto(null, 1, 100, '');
+  var pagination = PaginationDto(null, 1, 10, '');
   var vetApi = VetHttpApiRepository();
   final _searchController = TextEditingController();
   List<Vet> _especialistas = [];
   List<Vet> _filteredEspecialistas = [];
+  bool _hasMore = true;
 
   @override
   void initState() {
@@ -29,15 +32,25 @@ class _VetSearchPageState extends State<VetSearchPage> {
       _filterEspecialistas();
     });
   }
-  void getVets()async{
+
+  Future<void> _loadMoreVets() async {
+    if (_hasMore) {
+      pagination.recordsNumber+=10; // Increment page number
+      await getVets(); // Fetch more vets
+    }
+  }
+
+  Future<void> getVets() async {
     var response = await vetApi.getVets(pagination, '');
     if (response.wasSuccess) {
       setState(() {
         _especialistas = response.result!;
         _filteredEspecialistas = _especialistas;
+        _hasMore = response.result!.length == pagination.recordsNumber;
       });
     }
   }
+
   void _filterEspecialistas() {
     final searchTerm = _searchController.text.toLowerCase();
     if (searchTerm.isEmpty) {
@@ -51,7 +64,9 @@ class _VetSearchPageState extends State<VetSearchPage> {
       _filteredEspecialistas = _especialistas.where((especialista) {
         final fullNameLower = especialista.fullName.toLowerCase();
         final cityNameLower = especialista.cityName.toLowerCase();
-        final specialitiesLower = especialista.vetSpecialities.map((speciality) => speciality.name.toLowerCase()).toList();
+        final specialitiesLower = especialista.vetSpecialities
+            .map((speciality) => speciality.name.toLowerCase())
+            .toList();
 
         if (fullNameLower.contains(searchTerm) ||
             cityNameLower.contains(searchTerm)) {
@@ -83,36 +98,141 @@ class _VetSearchPageState extends State<VetSearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: TextField(
-          controller: _searchController,
-          decoration: const InputDecoration(
-            hintText: 'Nombre, especialidad o ciudad...',
-            suffixIcon: Icon(Icons.search),
+        appBar: AppBar(
+          title: TextField(
+            controller: _searchController,
+            decoration: const InputDecoration(
+              hintText: 'Nombre, especialidad o ciudad...',
+              suffixIcon: Icon(Icons.search),
+            ),
           ),
         ),
-      ),
-      body: _filteredEspecialistas != null && _filteredEspecialistas!.isNotEmpty
-      ? ListView.builder(
-        itemCount: _filteredEspecialistas.length,
-        itemBuilder: (context, index) {
-          final veterinario = _filteredEspecialistas[index];
-          var conse = Random().nextInt(100);
-          veterinario.address = 'https://picsum.photos/200/30$conse';
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundImage: Image.network(veterinario.address).image,
-            ),
-            title: Text(veterinario.fullName),
-            subtitle: Text(veterinario.vetSpecialities.isEmpty ? veterinario.cityName : '${veterinario.vetSpecialities.first.name} - ${veterinario.cityName}'),
-            trailing: const Icon(Icons.arrow_forward),
-            onTap: () => _navigateToVeterinarioDetails(veterinario),
-          );
-        },
-      ):
-      const Center(
-        child: CircularProgressIndicator(),
-      ),
-    );
+        body: NotificationListener<ScrollNotification>(
+            onNotification: (notification) {
+              if (notification is ScrollEndNotification &&
+                  notification.metrics.extentAfter == 0) {
+                _loadMoreVets(); // Load more vets when reaching the end of the list
+              }
+              return false;
+            },
+            child: ListView.builder(
+              itemCount: _filteredEspecialistas.length + (_hasMore ? 1 : 0),
+              itemBuilder: (context, index) {
+                if (index == _filteredEspecialistas.length) {
+                  if (_hasMore) {
+                    return const Center(
+                        child:
+                            CircularProgressIndicator()); // Display progress indicator while loading more
+                  } else {
+                    return const Center(
+                        child: Text(
+                            'No se encontraron más veterinarios')); // Display message when all vets are loaded
+                  }
+                }
+                final veterinario = _filteredEspecialistas[index];
+                var conse = Random().nextInt(100);
+                veterinario.address = 'https://picsum.photos/200/30$conse';
+                return Card(
+                  color: Colors.transparent,
+                  elevation: 0,
+                  child: InkWell(
+                    onTap: () => _navigateToVeterinarioDetails(veterinario),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            // Image and info row
+                            children: [
+                              CircleAvatar(
+                                backgroundImage:
+                                    Image.network(veterinario.address).image,
+                                radius: 30,
+                              ),
+                              const SizedBox(width: 16.0),
+                              Expanded(
+                                // Make info column flexible
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      // Row for name with icon
+                                      children: [
+                                        const Icon(Icons
+                                            .person_outline), // Add icon before name
+                                        const SizedBox(width: 8.0),
+                                        Text(veterinario.fullName,
+                                            style: const TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold)),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 4.0),
+                                    Row(
+                                      children: [
+                                        const Icon(Icons
+                                            .place_outlined), // Add icon before specialty
+                                        const SizedBox(width: 8.0),
+                                        Text(
+                                            veterinario.vetSpecialities.isEmpty
+                                                ? veterinario.cityName
+                                                : '${veterinario.vetSpecialities.first.name} - ${veterinario.cityName}',
+                                            style: const TextStyle(
+                                                fontSize: 14,
+                                                fontStyle: FontStyle.italic)),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 4.0),
+                                    Row(
+                                      children: [
+                                        const Icon(Icons.work_outline),
+                                        const SizedBox(width: 8.0),
+                                        Text(
+                                            veterinario.vetSpecialities.isEmpty
+                                                ? veterinario.cityName
+                                                : '${veterinario.vetSpecialities.first.name} - ${veterinario.cityName}',
+                                            style: const TextStyle(
+                                                fontSize: 14,
+                                                fontStyle: FontStyle.italic)),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 8.0),
+                                    Row(
+                                      children: [
+                                        const Icon(Icons.star_border_sharp),
+                                        const SizedBox(width: 8.0),
+                                        RatingBar.builder(
+                                          initialRating:
+                                              Random().nextDouble() * 5,
+                                          onRatingUpdate: (rating) {
+                                            // This callback is disabled since we don't want to edit the rating
+                                          },
+                                          minRating: 1,
+                                          maxRating: 5,
+                                          allowHalfRating: false,
+                                          ignoreGestures: true,
+                                          itemSize: 15,
+                                          itemCount: 5,
+                                          itemBuilder: (context, _) =>
+                                              const Icon(
+                                            Icons.star,
+                                            color: Colors.amber,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            )));
   }
 }
